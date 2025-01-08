e823fc61a5ce356f22e66728f07d1f45efe73b38
Akhil Gregory <bw974425@gmail.com>
2025-01-08 18:29:49 +0530

    Feature : Splash screen and Assets updated

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,74 @@ import 'screens/todo_dashboard.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final prefs = await SharedPreferences.getInstance();
-  String? loginToken = prefs.getString('loginToken');
-
-  runApp(MyApp(
-      initialRoute:
-          loginToken != null && loginToken.isNotEmpty ? 'dashboard' : 'login'));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String initialRoute;
-
-  const MyApp({super.key, required this.initialRoute});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: initialRoute,
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
       routes: {
-        'login': (context) => const LoginPage(),
-        'dashboard': (context) => const Dashboard(),
+        '/login': (context) => const LoginPage(),
+        '/dashboard': (context) => const Dashboard(),
       },
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToNextScreen();
+  }
+
+  Future<void> _navigateToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 3), () {});
+    
+    final prefs = await SharedPreferences.getInstance();
+    String? loginToken = prefs.getString('loginToken');
+    
+    if (mounted) {
+      Navigator.pushReplacementNamed(
+        context,
+        loginToken != null && loginToken.isNotEmpty ? '/dashboard' : '/login',
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF9395D2),
+              Color(0xFF99D0FF),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Image.asset(
+            'assets/images/logo.png',
+            width: 150,
+          ),
+        ),
+      ),
     );
   }
 }
